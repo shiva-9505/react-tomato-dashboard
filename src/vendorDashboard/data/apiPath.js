da95b8a538aa446ad740ebec836e6c956cefa61d
shivakumar <shivavadthyaclg9505@gmail.com>
2025-06-16 09:29:37 +0530

    changed location host path to live url

--- src/vendorDashboard/data/apiPath.js
+++ src/vendorDashboard/data/apiPath.js
@@ -1,3 +1,3 @@
-export const API_URL = "http://localhost:5000"
+// export const API_URL = "http://localhost:5000"
 
-//const API_URL = "https://backend-nodejs-multivendor-7mbl.onrender.com"
+const API_URL = "https://backend-nodejs-multivendor-7mbl.onrender.com"
