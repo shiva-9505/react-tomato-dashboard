b30311662e99b6bcae54b0d9226d81e1493fc6e0
shivakumar <shivavadthyaclg9505@gmail.com>
2025-06-16 09:39:24 +0530

    added export for API_URL

--- src/vendorDashboard/data/apiPath.js
+++ src/vendorDashboard/data/apiPath.js
@@ -1,3 +1,3 @@
 // export const API_URL = "http://localhost:5000"
 
-const API_URL = "https://backend-nodejs-multivendor-7mbl.onrender.com"
+export const API_URL = "https://backend-nodejs-multivendor-7mbl.onrender.com"
